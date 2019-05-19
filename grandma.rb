66d9223505fb4ca0a9a3d1fc707e0b0f1e0420ed
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-19 03:20:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,8 @@ def speak_to_grandma(speak)
   hear = "I LOVE YOU GRANDMA!"
   if (speak == hear && speak == speak.upcase)
     puts"I LOVE YOU TOO PUMPKIN!"
-  else if ( speak == speak.upcase)
+  elif( speak == speak.upcase)
     puts"NO, NOT SINCE 1938!"
-  else puts "HUH?! SPEAK UP, SONNY!"
   end
 
 end
